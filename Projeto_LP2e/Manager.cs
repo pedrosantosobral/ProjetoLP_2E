2696e76e31488015ef7b83476ecd7de8fd83b76d
Joana Marques <joana.pipaa@gmail.com>
2018-06-25 16:53:14 +0100

    Add class Manager

---
+++ Projeto_LP2e/Manager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Projeto_LP2e
+{
+    public class Manager
+    {
+        private readonly GameSetup gs;
+
+        public Manager(GameSetup gs)
+        {
+            this.gs = gs;
+        }
+
+        public void Play ()
+        {
+            Console.WriteLine(gs.X);
+            Console.WriteLine(gs.Y);
+            Console.WriteLine(gs.NZombies);
+            Console.WriteLine(gs.NHumans);
+            Console.WriteLine(gs.NPlayZombies);
+            Console.WriteLine(gs.NPlayHumans);
+            Console.WriteLine(gs.MaxTurns);
+        }
+    }
+}
